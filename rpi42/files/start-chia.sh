285f73860fc3c07a056d2e35b6f1859ac7d6e3d9
Maksym Pylypenko <max.pilipenko@gmail.com>
2022-01-31 00:42:21 +0200

    Store dump_rtsp script and systemd service file

--- rpi42/files/start-chia.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-PATH=$PATH:/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon
-whoami | systemd-cat -p info
-echo "${PATH}" | systemd-cat -p info
-chia start node harvester farmer wallet introducer | systemd-cat -p info
